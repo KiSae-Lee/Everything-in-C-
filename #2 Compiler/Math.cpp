f13d0dcc90695226da86a35ab76400e11d7d2d9a
KiSae-Lee <kisaelee.github@gmail.com>
2022-08-18 18:01:34 +0900

    Compiler added.

---
+++ #2 Compiler/Math.cpp
@@ -0,0 +1,20 @@
+int Plus(int a, int b)
+{
+	return a + b;
+}
+
+#define MYINT int
+
+MYINT Minus(MYINT a, MYINT b)
+{
+	return a - b;
+}
+
+#if 1
+
+int Multiply(int a, int b)
+{
+	return a * b;
+}
+
+#endif
